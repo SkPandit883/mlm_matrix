3f18ab370961d381bb6746c0b860528996c3dd35
santosh kumar pandit <skpandit659@gmail.com>
2021-10-10 17:03:27 +0545

    fix

--- app/Http/Controllers/MlmController.php
+++ app/Http/Controllers/MlmController.php
@@ -8,9 +8,17 @@ use App\Models\Matrix;
 class MlmController extends Controller
 {
    public function treeView(){
-       $matrix = Matrix::with(['user','left_child','middle_child','right_child'])->get();
-       $level_zero=Matrix::with(['user','left_child','middle_child','right_child'])->first();
-       return view('treeView',compact('matrix','level_zero'));
+       $data = array();
+       $child=array();
+       $temp=array();
+       $matrix = Matrix::with(['left_child','middle_child','right_child'])->get();
+       $level_zero=Matrix::where('level',0)->first();
+       $level_one=Matrix::where('level',1)->first();
+       $level_two=Matrix::where('level',2)->first();
+       $level_three=Matrix::where('level',3)->first();
+       
+       return view('treeView',compact('level_zero','level_one','level_two','level_three'));
+      
    }
    public function downline(){
        return view('downline');
--- app/Models/Matrix.php
+++ app/Models/Matrix.php
@@ -13,12 +13,13 @@ class Matrix extends Model
         return $this->belongsTo(User::class,'user_id');
     }
     public function left_child(){
-        return $this->belongsTo(User::class,'left_child');
+        return $this->hasOne(Matrix::class,'parent_id','left_child');
     }
     public function right_child(){
-        return $this->belongsTo(User::class,'right_child');
+        return $this->belongsTo(Matrix::class,'parent_id','right_child');
     }
     public function middle_child(){
-        return $this->belongsTo(User::class,'middle_child');
+        return $this->belongsTo(Matrix::class,'parent_id','middle_child');
     }
+   
 }
